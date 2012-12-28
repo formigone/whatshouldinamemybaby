ab6f2d69fd43583fa2bb3eb53fa8b9658134ad64
Rodrigo Silveira <webmaster@rodrigo-silveira.com>
2012-12-28 07:06:49 -0700

    Added very, very basic, crude UiBinder ui:style. Can't figure out how to override widget's behavioral style.

--- Name my Baby/src/com/formigone/namemybaby/views/BabyListView.java
+++ Name my Baby/src/com/formigone/namemybaby/views/BabyListView.java
@@ -2,7 +2,6 @@ package com.formigone.namemybaby.views;
 
 import java.util.List;
 
-import com.formigone.namemybaby.models.Baby;
 import com.formigone.namemybaby.models.Vote;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
@@ -11,8 +10,7 @@ import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.Label;
-import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.Widget;
 
 public class BabyListView extends Composite {
@@ -20,7 +18,7 @@ public class BabyListView extends Composite {
 	private static BabyListUiBinder uiBinder = GWT.create(BabyListUiBinder.class);
 	interface BabyListUiBinder extends UiBinder<Widget, BabyListView> {}
 
-	@UiField VerticalPanel vpBabyList;
+	@UiField FlowPanel vpBabyList;
 	
 	public BabyListView() {
 		initWidget(uiBinder.createAndBindUi(this));
